a02876dc30023e4473feb7102dc0857b2b156d87
Carwyn Stephen <cast@trustpilot.com>
2019-12-12 11:01:22 +0100

    Edit story component

--- stories/Modal.stories.js
+++ stories/Modal.stories.js
@@ -10,25 +10,10 @@ const Example = () => (
   <div style={{ padding: '120px' }}>I'm an example component</div>
 )
 
-const ManyExamples = () => (
-  <>
-    <Example />
-    <Example />
-    <Example />
-    <Example />
-    <Example />
-    <Example />
-    <Example />
-    <Example />
-    <Example />
-    <Example />
-  </>
-)
-
 const ModalInner = () => {
   const { showModal } = useModal()
 
-  return <button onClick={() => showModal(ManyExamples)}>Open a modal</button>
+  return <button onClick={() => showModal(Example)}>Open a modal</button>
 }
 
 export const defaultModal = () => {
